e4556e4c0a919b7977e75073bb3e2af8993b0a29
linken <linken@some.com>
2019-11-12 19:56:39 +0800

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DragSaveImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 496);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(997, 84);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 592);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "拖放图片直接保存";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace DragSaveImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.AllowDrop = true;
+            InitializeComponent();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string url = e.Data.GetData("System.String") as string;
+            Console.WriteLine(url);
+
+            String _dir = @"c:\_images\";
+            String prefix = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            String fn = _dir + prefix + ".tmp";
+
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(url, fn);
+
+            if (File.Exists(fn))
+            {
+                string ext = ValidFileType(fn);
+                if (ext != null)
+                {
+                    FileInfo fi = new FileInfo(fn);
+                    string new_fn = _dir + prefix + ext;
+                    fi.MoveTo(new_fn);
+                    textBox1.Text = "文件已经保存：" + new_fn;
+                }
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            string url = e.Data.GetData("System.String") as string;
+            Console.WriteLine(url);
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private string ValidFileType(string fileName)
+        {
+            string ext = null;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (System.IO.BinaryReader br = new System.IO.BinaryReader(fs))
+                {
+                    string fileType = string.Empty; ;
+                    byte data = br.ReadByte();
+                    fileType += data.ToString();
+                    data = br.ReadByte();
+                    fileType += data.ToString();
+                    FileExtension extension;
+                    extension = (FileExtension)Enum.Parse(typeof(FileExtension), fileType);
+                    if (extension == FileExtension.JPG)
+                    {
+                        ext = ".jpg";
+                    }
+
+                    if (extension == FileExtension.PNG)
+                    {
+                        ext = ".png";
+                    }
+
+                    return ext;
+                }
+            }
+        }
+
+        public enum FileExtension
+        {
+            JPG = 255216,
+            PNG = 13780,
+            BMP = 6677,
+            GIF = 7173,
+            COM = 7790,
+            EXE = 7790,
+            DLL = 7790,
+            RAR = 8297,
+            ZIP = 8075,
+            XML = 6063,
+            HTML = 6033,
+            ASPX = 239187,
+            CS = 117115,
+            JS = 119105,
+            TXT = 210187,
+            SQL = 255254,
+            BAT = 64101,
+            BTSEED = 10056,
+            RDP = 255254,
+            PSD = 5666,
+            PDF = 3780,
+            CHM = 7384,
+            LOG = 70105,
+            REG = 8269,
+            HLP = 6395,
+            DOC = 208207,
+            XLS = 208207,
+            DOCX = 208207,
+            XLSX = 208207,
+            VALIDFILE = 9999999
+        }
+    }
+}
